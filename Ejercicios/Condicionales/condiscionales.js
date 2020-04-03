3ed17aabc6b724f9feccf1dbdf7def18b2a42f7c
SandovalJr <edubizzle7@gmail.com>
2020-04-03 11:07:05 -0600

    finalizado:

---
+++ Ejercicios/Condicionales/condiscionales.js
@@ -0,0 +1,25 @@
+let edad = 18;
+
+if (edad >= 18) {
+  console.log("si puedes entrar");
+} else {
+  console.log("no puedes entrar al ciclo");
+}
+
+let puntaje = 3000;
+
+if (puntaje) {
+  console.log(`El puntaje fue de ${puntaje}`);
+} else {
+  console.log("no hay puntaje");
+}
+
+let tcompra = 600;
+let efectivo = 500;
+
+if (tcompra < efectivo) {
+  console.log("Pagando...");
+} else {
+  let resta = tcompra - efectivo;
+  console.log("No cumples con el dinero suficiente te faltan " + resta);
+}
