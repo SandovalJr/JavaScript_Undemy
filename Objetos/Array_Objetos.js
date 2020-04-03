8e4b459e7d916a2d53c2fc77cc6c79058697735c
SandovalJr <edubizzle7@gmail.com>
2020-04-03 09:51:49 -0600

    listo

---
+++ Objetos/Array_Objetos.js
@@ -0,0 +1,19 @@
+// Array de objetos
+const autos = [
+  {
+    modelo: "Mustang",
+    motor: 6.0,
+  },
+  {
+    modelo: "camaro",
+    motor: 6.0,
+  },
+];
+
+// for (let x = 0; x < autos.length; x++){
+//     console.log(autos[x]);
+// }
+
+autos[0] = "Audi";
+
+console.log(autos);
